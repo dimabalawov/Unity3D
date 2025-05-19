38e78fdffec759757d40b4c5bd5ab18472985811
dimab <dimab@LEGION5>
2025-05-19 22:01:24 +0300

    Применён .gitignore

--- Assets/Scripts/EffectsSounds.cs
+++
@@ -1,6 +0,0 @@
-public class EffectsSounds
-{
-    public const string KeyCollectedInTime = nameof(KeyCollectedInTime);
-    public const string KeyCollectedOutOfTime = nameof(KeyCollectedOutOfTime);
-    public const string BatteryCollected = nameof(BatteryCollected);
-}
--- Assets/Scripts/GameEvent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GameEvent
-{
-    public string type { get; set; }
-    public object payload { get; set; }
-    public string toast { get; set; }
-    public string sound { get; set; }
-}
